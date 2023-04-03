723a58f9d0b0db5e09b94ffd7e70a170ee541538
Jason <ggj00125@gmail.com>
2023-04-03 13:19:10 +0900

    feat: collectionView 초기 구현

--- WeatherForecast/WeatherForecast/Presentation/View/WeatherForecastView.swift
+++ WeatherForecast/WeatherForecast/Presentation/View/WeatherForecastView.swift
@@ -11,10 +11,45 @@ class WeatherForecastView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        createLayout()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         fatalError("Can't drawing collectioView")
     }
+    
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        
+        collectionView.isScrollEnabled = true
+        collectionView.clipsToBounds = true
+        
+        collectionView.register(WeatherForecastHeaderView.self,
+                                forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+                                withReuseIdentifier: WeatherForecastHeaderView.identifier)
+        
+        return collectionView
+    }()
+    
+    func setDataSource(_ dataSource: UICollectionViewDataSource) {
+        collectionView.dataSource = dataSource
+    }
+    
+    func setDelegate(_ delegate: UICollectionViewDelegate) {
+        collectionView.delegate = delegate
+    }
+    
+    private func createLayout() {
+        addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+    //TODO: - CompositionalLayout 구현 예정
 }
