8e4e508a75f1414ee30565fcb7a8cac57d7f5dd2
Jason <ggj00125@gmail.com>
2023-04-03 13:13:29 +0900

    Feat: WeatherForecastView 파일 생성 및 초기구현

---
+++ WeatherForecast/WeatherForecast/Presentation/View/WeatherForecastView.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherForecastView.swift
+//  WeatherForecast
+//
+//  Created by Jason on 2023/04/03.
+//
+
+import UIKit
+
+class WeatherForecastView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        fatalError("Can't drawing collectioView")
+    }
+}
